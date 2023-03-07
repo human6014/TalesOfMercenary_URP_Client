15650046c9c95af1adbf7a64af3c4481eccefc31
권승현 <84135346+human6014@users.noreply.github.com>
2023-03-08 02:47:26 +0900

    Feat : Shop UI, Separate scene, Style : Hierarchy UI
    상점 Ui구현 완료
    로그인, 로비 씬 분리
    하이라키 뷰 UI구조 변경
    // 로비 씬에서 게임 씬 갈 때 조명 이상함

--- Assets/UserAssets/Script/Lobby/PanelManager.cs
+++ Assets/UserAssets/Script/Lobby/PanelManager.cs
@@ -2,84 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-/// <summary>
-/// Panel끼리 이동만 담당함 (Scene 이동은 LobbyManager가 담당)
-/// </summary>
 public class PanelManager : MonoBehaviour
 {
-    [SerializeField] private GameObject loginPanel;
-    [SerializeField] private GameObject registerPanel;
-    [SerializeField] private GameObject lobbyPanel;
-    [SerializeField] private GameObject banPickPanel;
+    [SerializeField] private GameObject lobbyCanvas;
+    [SerializeField] private GameObject banPickCanvas;
+    [SerializeField] private GameObject shopCanvas;
     [SerializeField] private GameObject deckPanel;
-
-    [SerializeField] private Text loginIDText;
-    [SerializeField] private Text loginPWText;
-
-    [SerializeField] private Text registerIDText;
-    [SerializeField] private Text registerPWText;
-
-    private GameObject currentPanel;
-
+    
+    private GameObject currentCanvas;
+    private BanPickManager banPickManager;
     private void Awake()
     {
-        currentPanel = loginPanel;
-    }
-
-    /// <summary>
-    /// 로그인 화면으로 돌아가기
-    /// </summary>
-    public void SetLoginPanel(bool isRegister)
-    {
-        if (isRegister)
-        {
-            if (string.IsNullOrEmpty(registerIDText.text) || string.IsNullOrEmpty(registerPWText.text))
-            {
-                return;
-            }
-        }
-        currentPanel.SetActive(false);
-        loginPanel.SetActive(true);
-        currentPanel = loginPanel;
-    }
-
-    /// <summary>
-    /// 회원가입 화면으로 가기
-    /// </summary>
-    public void SetRegisterPanel()
-    {
-        currentPanel.SetActive(false);
-        registerPanel.SetActive(true);
-        currentPanel = registerPanel;
+        currentCanvas = lobbyCanvas;
+        banPickManager = FindObjectOfType<BanPickManager>();
     }
 
     /// <summary>
     /// 로비 화면 가기
     /// </summary>
-    public void SetLobbyPanel(bool isLogin)
+    public void SetLobbyCanvas()
     {
-        if (isLogin)
-        {
-            if (string.IsNullOrEmpty(loginIDText.text) || string.IsNullOrEmpty(loginPWText.text))
-            {
-                return;
-            }
-        }
-        currentPanel.SetActive(false);
-        lobbyPanel.SetActive(true);
-        currentPanel = lobbyPanel;
+        currentCanvas.SetActive(false);
+        lobbyCanvas.SetActive(true);
+        currentCanvas = lobbyCanvas;
     }
 
     /// <summary>
     /// 매칭 잡힌 후 밴픽 화면 가기
     /// </summary>
-    public void SetBanPickPanel()
+    public void SetBanPickCanvas()
     {
-        currentPanel.SetActive(false);
-        banPickPanel.SetActive(true);
-        currentPanel = banPickPanel;
+        currentCanvas.SetActive(false);
+        banPickCanvas.SetActive(true);
+        currentCanvas = banPickCanvas;
+
+        banPickManager.SetCardList();
     }
 
-
+    /// <summary>
+    /// 상점 화면 가기
+    /// </summary>
+    public void SetShopCanvas()
+    {
+        currentCanvas.SetActive(false);
+        shopCanvas.SetActive(true);
+        currentCanvas = shopCanvas;
+    }
 }
