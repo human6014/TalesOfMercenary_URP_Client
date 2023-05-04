8908ebd6479f2919837b3e46eeff8b0d294a6922
Hyup98 <40859624+Hyup98@users.noreply.github.com>
2023-05-04 14:20:27 +0900

    확률표 일단 올림 정확히 어떤 배열을 원하는지 몰라서

---
+++ Assets/UserAssets/Script/Game/Unit/Preference/Probability.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class Probability : MonoBehaviour
+{
+    float[] BuildingLV1 = new float[3] { 90f, 10f, 0f };
+    float[] BuildingLV2 = new float[3] { 50f, 49f, 1f };
+    float[] BuildingLV3 = new float[3] { 35f, 60f, 5f };
+    float[] BuildingLV4 = new float[3] { 20f, 70f, 10f };
+}
